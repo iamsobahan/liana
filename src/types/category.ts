70edf0c8982e78e49089ad40cb0a58a7c385f0d3
jobair-talha <talhajobair10@gmail.com>
2025-09-08 10:34:30 +0600

    add type definitions for API responses, categories, products, and sliders

---
+++ src/types/category.ts
@@ -0,0 +1,17 @@
+export type ICategory = {
+    parentId: string | null;
+    name: string;
+    slug: string;
+    image: string;
+    isFeatured: boolean;
+    adsBanner: string;
+    metaTitle: string;
+    metaDescription: string;
+    _id: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+    id: string;
+    children?: ICategory[];
+
+};
