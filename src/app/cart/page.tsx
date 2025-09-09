4e0d542ac70f0fbe5dab1e96eed4fe8053e993ac
jobair-talha <talhajobair10@gmail.com>
2025-09-10 03:14:29 +0600

    Refactor CartPage component to enhance item rendering and state management; update subtotal and checkout button display

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,85 +1,38 @@
 // app/cart/page.tsx (Next.js 13+ App Router)
-'use client';
-import {  FaMinus, FaPlus } from 'react-icons/fa';
-import { HiOutlineTrash } from 'react-icons/hi';
-import slick from '../../assets/slick.jpeg'
-import slick1 from '../../assets/slick1.jpeg'
-import Image from 'next/image';
-
-function CartItem({
-  title,
-  brand,
-  size,
-  price,
-  oldPrice,
-  quantity,
-}: {
-  title: string;
-  brand: string;
-  size: string;
-  price: number;
-  oldPrice: number;
-  quantity: number;
-}) {
-  return (
-    <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 border-b border-gray-300 pb-4">
-      {/* Left */}
-      <div className="flex items-center  gap-4 w-full sm:w-auto">
-        <input
-          type="checkbox"
-          className="w-5 h-5 appearance-none border-2 border-gray-400 rounded-sm checked:before:content-['✔'] checked:before:text-orange-500 checked:before:block checked:before:text-center checked:before:text-sm focus:outline-none cursor-pointer"
-        />
-
-        <Image
-          src={slick}
-          alt="slick"
-          className="w-20 h-20 object-contain rounded"
-        />
-
-        <div className="flex flex-col">
-          <h2 className="whitespace-break-spaces text-sm sm:text-base font-medium">
-            {title}
-          </h2>
-          <p className="whitespace-break-spaces text-xs text-gray-500">
-            {brand}, {size}
-          </p>
-          <span className="mt-1 text-xs  text-orange-600 py-0.5 rounded">
-            BEST PRICE
-          </span>
-        </div>
-      </div>
-
-      {/* Right */}
-      <div className="flex flex-col sm:flex-row items-start sm:items-center gap-3 w-full sm:w-auto">
-        {/* Price */}
-        <div className="flex flex-col">
-          <p className="text-orange-600 font-semibold text-lg ">{price}৳</p>
-          <p className="line-through text-gray-400 text-sm"> {oldPrice}৳</p>
-        </div>
-
-        {/* Quantity */}
-        <div className="flex items-center border rounded">
-          <button className="px-2 py-1 hover:bg-gray-100">
-            <FaMinus size={14} />
-          </button>
-          <span className="px-3">{quantity}</span>
-          <button className="px-2 py-1 hover:bg-gray-100">
-            <FaPlus size={14} />
-          </button>
-        </div>
-
-        {/* Wishlist + Delete */}
-        <div className="flex items-center gap-3">
-          <button className="text-gray-500 hover:text-red-500 cursor-pointer">
-            <HiOutlineTrash size={25} />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
+"use client";
+import { FaMinus, FaPlus } from "react-icons/fa";
+import { HiOutlineTrash } from "react-icons/hi";
+import Image from "next/image";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import Link from "next/link";
+import config from "@/config";
+import { ICartItem } from "@/types/cart";
+import {
+  removeFromCart,
+  incrementQuantity,
+  decrementQuantity,
+} from "@/redux/features/cart/cartSlice";
 
 export default function CartPage() {
+  const { cart: cartItems } = useAppSelector((state) => state.cart);
+  const dispatch = useAppDispatch();
+
+  const totalPrice = cartItems.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
+  const handleDelete = (product: ICartItem) => {
+    dispatch(removeFromCart(product));
+  };
+
+  const handleIncrementQuantity = (product: ICartItem) => {
+    dispatch(incrementQuantity(product));
+  };
+
+  const handleDecrementQuantity = (product: ICartItem) => {
+    dispatch(decrementQuantity(product));
+  };
+
   return (
     <div className="max-w-7xl mx-auto p-4 grid grid-cols-1 lg:grid-cols-3 gap-6 text-gray-800">
       {/* LEFT SIDE */}
@@ -91,7 +44,7 @@ export default function CartPage() {
               type="checkbox"
               className="w-5 h-5 appearance-none border-2 border-gray-400 rounded-sm checked:before:content-['✔'] checked:before:text-orange-500 checked:before:block checked:before:text-center checked:before:text-sm focus:outline-none cursor-pointer"
             />
-            SELECT ALL (6 ITEM(S))
+            SELECT ALL ({cartItems.length} ITEM(S))
           </label>
           <button className="flex items-center gap-1 text-gray-600 fw-bold hover:underline hover:text-red-500 cursor-pointer">
             <HiOutlineTrash size={25} />
@@ -99,22 +52,79 @@ export default function CartPage() {
         </div>
 
         {/* Items */}
-        <CartItem
-          title="Rajkonna Light Moisturizer With Rice Water And Licorice Extract (50gm)"
-          brand="RAJKONNA"
-          size="Scent: Fresh"
-          price={250}
-          oldPrice={295}
-          quantity={1}
-        />
-        <CartItem
-          title="Rajkonna Moisturizing Shower Gel - 330ml"
-          brand="RAJKONNA"
-          size="Scent: Fresh"
-          price={280}
-          oldPrice={330}
-          quantity={1}
-        />
+        {cartItems.length === 0 ? (
+          <p className="text-center text-gray-500 py-20">Your cart is empty.</p>
+        ) : (
+          cartItems.map((item) => (
+            <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 border-b border-gray-300 pb-4">
+              {/* Left */}
+              <div className="flex items-center  gap-4 w-full sm:w-auto">
+                <input
+                  type="checkbox"
+                  className="w-5 h-5 appearance-none border-2 border-gray-400 rounded-sm checked:before:content-['✔'] checked:before:text-orange-500 checked:before:block checked:before:text-center checked:before:text-sm focus:outline-none cursor-pointer"
+                />
+
+                <Image
+                  src={`${config.API_URL}/images/products/${item.image}`}
+                  alt={item.title}
+                  width={80}
+                  height={80}
+                  className="w-20 h-20 object-contain rounded"
+                />
+
+                <div className="flex flex-col">
+                  <h2 className="whitespace-break-spaces text-sm sm:text-base font-medium">
+                    {item.title}
+                  </h2>
+                  <span className="mt-1 text-xs  text-orange-600 py-0.5 rounded">
+                    BEST PRICE
+                  </span>
+                </div>
+              </div>
+
+              {/* Right */}
+              <div className="flex flex-col sm:flex-row items-start sm:items-center gap-3 w-full sm:w-auto">
+                {/* Price */}
+                <div className="flex flex-col">
+                  <p className="text-orange-600 font-semibold text-lg ">
+                    {item.price}৳
+                  </p>
+                  <p className="line-through text-gray-400 text-sm">
+                    {" "}
+                    {item.regularPrice}৳
+                  </p>
+                </div>
+
+                {/* Quantity */}
+                <div className="flex items-center border rounded">
+                  <button
+                    className="px-2 py-1 hover:bg-gray-100"
+                    onClick={() => handleDecrementQuantity(item)}
+                  >
+                    <FaMinus size={14} />
+                  </button>
+                  <span className="px-3">{item.quantity}</span>
+                  <button
+                    className="px-2 py-1 hover:bg-gray-100"
+                    onClick={() => handleIncrementQuantity(item)}
+                  >
+                    <FaPlus size={14} />
+                  </button>
+                </div>
+
+                {/*  Delete */}
+                <div className="flex items-center gap-3">
+                  <button
+                    onClick={() => handleDelete(item)}
+                    className="text-gray-500 hover:text-red-500 cursor-pointer"
+                  >
+                    <HiOutlineTrash size={25} />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
       </div>
 
       {/* RIGHT SIDE */}
@@ -123,22 +133,24 @@ export default function CartPage() {
 
         <div className="flex flex-col text-md mb-2">
           <div className="flex justify-between">
-            <p className="text-md">Subtotal (0 items)</p>
-            <p className="text-md">৳ 0</p>
+            <p className="text-md">Subtotal ({cartItems.length} items)</p>
+            <p className="text-md">৳ {totalPrice}</p>
           </div>
-          <small>(Shipping fee included)</small>
+          <small>+ (Shipping fee included)</small>
         </div>
 
         {/* Total */}
         <div className="flex justify-between text-lg font-semibold mb-4">
           <span>Sub Total</span>
-          <span>৳ 0</span>
+          <span>৳ {totalPrice}</span>
         </div>
 
         {/* Checkout Button */}
-        <button className="w-full bg-orange-500 text-white fw-semibold py-3 rounded hover:bg-orange-600">
-          PROCEED TO CHECKOUT (0)
-        </button>
+        <Link href="/checkout">
+          <button className="w-full bg-orange-500 text-white fw-semibold py-3 rounded hover:bg-orange-600">
+            PROCEED TO CHECKOUT ({cartItems.length} ITEM(S))
+          </button>
+        </Link>
       </div>
     </div>
   );
